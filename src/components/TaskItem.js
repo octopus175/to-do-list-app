2ed9979aec56494193d5f7e484cf0641c9492781
octopus175 <promolihkg@gmail.com>
2022-08-05 23:43:53 -0400

    Add deadline date to item

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -3,6 +3,7 @@ function TaskItem() {
         <div>
             <span>this is a task |</span>
             <span> pending | </span>
+            <span>02/08/2023 08:30pm | </span>
             <button>check</button>
         </div>
         
