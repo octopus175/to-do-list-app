38f887d7ef756e70dcfe0f99be0916b4f3cd25ce
octopus175 <promolihkg@gmail.com>
2022-08-16 01:57:22 -0400

    Add trash can icon

--- src/components/CreateNewItem.js
+++ src/components/CreateNewItem.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import '../style/CreateNewItem.css';
 import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function CreateNewItem({insertNewItem}){
     const [taskName, setTaskName] = useState('');
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -2,6 +2,8 @@ import '../style/TaskItem.css'
 
 function TaskItem({taskItem, taskId, completeTaskItem}) {
 
+    const deleteStyle = {'font-size': '2em'};
+
     const deadline = taskItem.deadline.replace('T',' ').replace('Z',' ');
     return(
         <div className="item-container">
@@ -9,6 +11,10 @@ function TaskItem({taskItem, taskId, completeTaskItem}) {
             <span className='task-status'>{taskItem.status}</span>
             <span className='task-deadline'>{deadline}</span>
             <button className="complete-button" onClick={() => completeTaskItem(taskId)}>Complete</button>
+            <button className='delete-button'>
+                <i class="bi bi-trash3" style={deleteStyle}></i>
+            </button>
+            
         </div>
         
     )
