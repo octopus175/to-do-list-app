cb8b8aee7049147b630aa4a115475eb5bb67cf3a
octopus175 <promolihkg@gmail.com>
2022-08-16 02:27:50 -0400

    Add feature: change item status after clicking complete button

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -5,6 +5,7 @@ function TaskItem({taskItem, taskId, deleteTaskItem}) {
 
     const deleteStyle = {'fontSize': '2em'};
 
+    const [taskStatus, setTaskStatus] = useState(false)
 
     const [nameStyle, setNameStyle] = useState({textDecoration: "none"})
 
@@ -12,9 +13,9 @@ function TaskItem({taskItem, taskId, deleteTaskItem}) {
     return(
         <div className="item-container">
             <span className='task-name' style={nameStyle}>{taskItem.name}</span>
-            <span className='task-status'>{taskItem.status}</span>
+            {taskStatus?(<span className='task-status'>Complete</span>):(<span className='task-status'>In progess</span>)}
             <span className='task-deadline'>{deadline}</span>
-            <button className="complete-button" onClick={() => setNameStyle({textDecoration: "line-through"})}>Complete</button>
+            <button className="complete-button" onClick={() => {setNameStyle({textDecoration: "line-through"}); setTaskStatus(true)}}>Complete</button>
             <button className='delete-button'>
                 <i className="bi bi-trash3" style={deleteStyle} onClick={() => deleteTaskItem(taskId)}></i>
             </button>
