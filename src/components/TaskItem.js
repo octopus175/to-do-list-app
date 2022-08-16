c9e0dfdcab88532fa090ecdf8db81b7b4702ed55
octopus175 <promolihkg@gmail.com>
2022-08-16 02:20:53 -0400

    Add feature: strikethrough task name after pressing complete button

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,18 +1,22 @@
+import { useState } from 'react';
 import '../style/TaskItem.css'
 
 function TaskItem({taskItem, taskId, deleteTaskItem}) {
 
     const deleteStyle = {'fontSize': '2em'};
 
+
+    const [nameStyle, setNameStyle] = useState({textDecoration: "none"})
+
     const deadline = taskItem.deadline.replace('T',' ').replace('Z',' ');
     return(
         <div className="item-container">
-            <span className='task-name'>{taskItem.name}</span>
+            <span className='task-name' style={nameStyle}>{taskItem.name}</span>
             <span className='task-status'>{taskItem.status}</span>
             <span className='task-deadline'>{deadline}</span>
-            <button className="complete-button" onClick={() => deleteTaskItem(taskId)}>Complete</button>
+            <button className="complete-button" onClick={() => setNameStyle({textDecoration: "line-through"})}>Complete</button>
             <button className='delete-button'>
-                <i className="bi bi-trash3" style={deleteStyle}></i>
+                <i className="bi bi-trash3" style={deleteStyle} onClick={() => deleteTaskItem(taskId)}></i>
             </button>
             
         </div>
