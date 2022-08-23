2e03f805506fea30dd88c09e430c2d981921c938
octopus175 <promolihkg@gmail.com>
2022-08-23 19:27:44 -0400

    [client] Add: create services for db interactions

---
+++ client/src/services/taskServices.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+const api_url = "http://127.0.0.1:4000/api/tasks/";
+
+export function getTasks() {
+    return axios.get(api_url.concat("getTasks"));
+}
+
+export function addTask(newTask) {
+    return axios.post(api_url.concat("addTask"), newTask);
+}
+
+export function updateTask(task) {
+    return axios.patch(api_url.concat("updateTask"), task);
+}
+
+export function deleteTask(taskId) {
+    return axios.delete(api_url.concat("deleteTask"), taskId);
+}
