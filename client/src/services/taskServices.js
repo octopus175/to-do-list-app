091fc2045a0d57cc465431ec2fbad71d901c9c48
octopus175 <promolihkg@gmail.com>
2022-08-24 15:56:17 -0400

    [client] Add: use .env to save api uri

--- client/src/services/taskServices.js
+++ client/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api_url = "http://127.0.0.1:4000/api/tasks/";
+const api_url = process.env.REACT_APP_TASK_SERVICE_URI;
 
 export function getTasks() {
     return axios.get(api_url.concat("getTasks"));
