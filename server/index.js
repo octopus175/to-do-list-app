3b53c7c24692f1c02f6777d828b65cc6d5777f4a
octopus175 <promolihkg@gmail.com>
2022-08-30 06:28:35 -0400

    [Client] Change: default port number

--- server/index.js
+++ server/index.js
@@ -12,5 +12,5 @@ app.use(cors());
 
 app.use("/api/tasks", tasks);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 app.listen(port, ()=> console.log(`Server listening on ${port}...`));
